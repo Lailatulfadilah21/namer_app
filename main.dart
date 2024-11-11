04ad0f9bc83ef43488d216992e1d03f4e181e025
Lailatulfadilah21 <fadilahlailatul045@gmail.com>
2024-11-11 10:31:28 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -8,90 +8,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login Page"),
-        backgroundColor: Colors.blue,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                'logo.png',
-                height: 100,
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(), // Membuat kolom menjadi kotak
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Container Widget'),
+        ),
+        body: Center(
+          child: Container(
+            height: 320,
+            width: double.infinity,
+            margin: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 219, 218, 157),
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: const Color.fromARGB(255, 171, 183, 58)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 8,
+                  offset: const Offset(0, 4),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              SizedBox(width: 10,),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(), // Membuat kolom menjadi kotak
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset('assets/flutter_gambar_dica.jpeg', height: 50),
+                const SizedBox(height: 8),
+                const Text(
+                  'Dila dan Ica',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-
-              
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // Process the form data
-                  }
-                },
-                child: Text('Login'),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/forgetPassword');
-                }, child: Text('Forgot Password'),
-              ),
-              SizedBox(height: 20),
-            
-              TextButton(
-                onPressed: (){
-                  Navigator.pushNamed(context, 'register');
-                },
-                 child: Text('Don\'t have an account? Register'),
-              ),
-            ],
+                const SizedBox(height: 8),
+                const Divider(color: Colors.black),
+                const Text(
+                  'Quotes Hari ini: Dibalik senyum yang tampak kuat, sebenarnya ada cerita yang tak terucap. Terlihat kuat kadang hanya cara untuk bertahan, bukan tanda semuanya baik-baik saja',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 40,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('klik untuk quotes lain'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
